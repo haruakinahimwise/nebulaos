c2ec271767b3ad168bdc76cd903a1b57ee087a04
haruakinahimwise <haruakinahimwise@gmail.com>
2026-05-28 08:39:53 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/sw/',
-    bare: '/bare/',
+    prefix: "/nebulaos/uv/",
+    bare: "/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: "/nebulaos/uv/uv.handler.js",
+    bundle: "/nebulaos/uv/uv.bundle.js",
+    config: "/nebulaos/uv/uv.config.js",
+    sw: "/nebulaos/uv/uv.sw.js",
 };
